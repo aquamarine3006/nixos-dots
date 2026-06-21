fa598ef4e7368f14460a43176a5bcee995048741
aquamarine3006 <pavkar02@gmail.com>
2026-06-21 20:57:16 +0530

    system: restore full flake inputs

--- flake.nix
+++ flake.nix
@@ -4,18 +4,45 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    hyprland = {
+      url = "github:hyprwm/Hyprland";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    quickshell = {
+      url = "github:quickshell-mirror/quickshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     pantheon-sddm = {
       url = "github:OminduD/PantheonSDDM";
       flake = false;
     };
   };
 
-  outputs = { self, nixpkgs, pantheon-sddm, ... }@inputs:
+  outputs = { self, nixpkgs, home-manager, hyprland, quickshell,
+               pantheon-sddm, ... }@inputs:
   {
     nixosConfigurations.aqua = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
-      modules = [ ./hosts/aqua/default.nix ];
+      modules = [
+        ./hosts/aqua/default.nix
+
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.extraSpecialArgs = { inherit inputs; };
+          home-manager.users.aqua = import ./home/default.nix;
+          home-manager.backupFileExtension = "bak";
+        }
+      ];
     };
   };
 }
