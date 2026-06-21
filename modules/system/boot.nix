f2ee0a261ac979eb1fe6e54315668f663d276e3f
aquamarine3006 <pavkar02@gmail.com>
2026-06-21 20:02:05 +0530

    system: fix plymouth paths and enable amd modesetting

--- modules/system/boot.nix
+++ modules/system/boot.nix
@@ -1,35 +1,49 @@
-{ pkgs, inputs, ... }:
+{ pkgs, inputs, lib, ... }:
+
 let
   proxzima = pkgs.stdenvNoCC.mkDerivation {
     name = "proxzima-plymouth";
-    src  = inputs.proxzima-plymouth;
+    src = inputs.proxzima-plymouth;
+    
+    dontBuild = true;
+    
     installPhase = ''
+      runHook preInstall
       mkdir -p $out/share/plymouth/themes/proxzima
-      cp -r proxzima/. $out/share/plymouth/themes/proxzima/
-      sed -i "s|/usr|$out|g" \
-        $out/share/plymouth/themes/proxzima/*.plymouth 2>/dev/null || true
+      cp -r proxzima/* $out/share/plymouth/themes/proxzima/
+      
+      # Use Nix's native substituteInPlace to guarantee the image paths point to the Nix store
+      if [ -f "$out/share/plymouth/themes/proxzima/proxzima.plymouth" ]; then
+        substituteInPlace $out/share/plymouth/themes/proxzima/proxzima.plymouth \
+          --replace "/usr/share/plymouth/themes/proxzima" "$out/share/plymouth/themes/proxzima"
+      fi
+      runHook postInstall
     '';
   };
 in
 {
   boot = {
-    loader.systemd-boot.enable      = true;
+    loader.systemd-boot.enable = true;
     loader.efi.canTouchEfiVariables = true;
-    kernelPackages                  = pkgs.linuxPackages_latest;
+    kernelPackages = pkgs.linuxPackages_latest;
 
-    plymouth = {
-      enable        = true;
-      themePackages = [ proxzima ];
-      theme         = "proxzima";
-    };
-
-    consoleLogLevel = 0;
-    initrd.verbose  = false;
-    kernelParams    = [
+    # 1. CRITICAL FOR AMD: Enable early modesetting so Plymouth can draw immediately
+    initrd.kernelModules = [ "amdgpu" ];
+    kernelParams = [
       "quiet"
       "splash"
-      "udev.log_level=0"
-      "rd.udev.log_level=0"
+      "amdgpu.modeset=1"
+      "rd.udev.log_level=3"
+      "rd.systemd.show_status=auto"
     ];
+
+    consoleLogLevel = 0;
+    initrd.verbose = false;
+
+    plymouth = {
+      enable = true;
+      themePackages = [ proxzima ];
+      theme = "proxzima";
+    };
   };
 }
